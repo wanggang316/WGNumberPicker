7f7ff04419f0059a76c2adc9107b8069a3f0d08e
wlgg <wgang316@163.com>
2015-10-19 16:12:57 +0800

    Initial Commit

---
+++ WGNumberPicker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WGNumberPicker
+//
+//  Created by 王刚 on 15/10/19.
+//  Copyright © 2015年 王刚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WGNumberPicker/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WGNumberPicker
+//
+//  Created by 王刚 on 15/10/19.
+//  Copyright © 2015年 王刚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
